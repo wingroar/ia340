f15fb1d90b5ba997037c8b982e5b9d1da401f33b
wingroar <122937173+wingroar@users.noreply.github.com>
2023-09-11 16:25:43 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -1,1 +1,8 @@
+-- Q2.1
+--insert into student(s_email,s_name,major)
+--values('s1@jmu.edu','s1','IA'),
+--('s2@jmu.edu','s2','IA'),
+--('s3@jmu.edu','s3','ISAT'),
+--('s4@jmu.edu','s4','ISAT')
 
+select * from student
