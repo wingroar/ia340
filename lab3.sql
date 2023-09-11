51092dadbf7423c6c8ef3e1f029799eb1b1625a5
wingroar <122937173+wingroar@users.noreply.github.com>
2023-09-11 16:36:22 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -5,4 +5,10 @@
 --('s3@jmu.edu','s3','ISAT'),
 --('s4@jmu.edu','s4','ISAT')
 
-select * from student
+--select * from student
+
+--insert into professor(p_email,p_name,office)
+--values('p1@jmu.edu','p1','o1'),
+--('p2@jmu.edu','p2','o2')
+
+select * from professor
