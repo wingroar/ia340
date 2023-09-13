f3f814b8096db783c6c332a347b790b0ff7c14da
wingroar <122937173+wingroar@users.noreply.github.com>
2023-09-13 16:26:41 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -5,10 +5,37 @@
 --('s3@jmu.edu','s3','ISAT'),
 --('s4@jmu.edu','s4','ISAT')
 
---select * from student
-
---insert into professor(p_email,p_name,office)
+-- Q2.2
+--insert into professor (p_email,p_name,office)
 --values('p1@jmu.edu','p1','o1'),
---('p2@jmu.edu','p2','o2')
+--('p2jmu.edu','p2','o2')
 
-select * from professor
+-- Q2.3
+--insert into course (c_number,c_name,room,p_email)
+--values('c1','postgresql','r1','p1@jmu.edu'),
+--('c2','mongodb','r2','p2@jmu.edu'),
+--('c3','twitter','r1','p1@jmu.edu')
+
+-- Q2.4
+--insert into enroll(s_email,c_number)
+--values('s1@jmu.edu','c1'),
+--('s2@jmu.edu','c1'),
+--('s3@jmu.edu','c1'),
+--('s4@jmu.edu','c2'),
+--('s2@jmu.edu','c3'),
+--('s3@jmu.edu','c3')
+
+-- Q2.5
+--insert into professor(p_email,p_name,office)
+--values('p3@jmu.edu','p3','o3');
+
+--insert into course(c_number,c_name,room,p_email)
+--values('c4','facebook','r1','p3@jmu.edu')
+
+-- Q2.6
+--update course
+--set p_email = 'p3@jmu.edu'
+--where p_email = 'p1@jmu.edu';
+
+--delete from professor
+--where p_email = 'p1@jmu.edu'
